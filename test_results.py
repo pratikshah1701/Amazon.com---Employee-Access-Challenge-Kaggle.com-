860698da1f33cc33965d7f2605a23217e9f83942
pratikshah1701 <pratikshah1701@gmail.com>
2017-04-09 18:20:32 -0500

    Update test_results.py

--- test_results.py
+++ test_results.py
@@ -4,9 +4,9 @@ import pandas as pd
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df_train= pd.read_csv('https://raw.githubusercontent.com/vidushidikshit/Machine_Learning/master/train_amazon.csv')
+df_train= pd.read_csv('https://raw.githubusercontent.com/pratikshah1701/Amazon.com---Employee-Access-Challenge-Kaggle.com-/master/train.csv')
 #df_train
-df_threshold = pd.read_csv('0.3.csv')
+df_threshold = pd.read_csv('0.3.csv') 
 #print(df_threshold)
 df_threshold.plot(x='RESOURCE', y='ACTION', style='ro')
 df_train.plot(x='RESOURCE', y='ACTION', style='o')
